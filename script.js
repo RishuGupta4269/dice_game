659fecc0430f9d4df0fe423f53cd0c8b1d4e55d9
Rishu Gupta <rishugupta.4269@gmail.com>
2021-02-27 14:46:38 +0530

    Functionalities added

---
+++ script.js
@@ -0,0 +1,34 @@
+var play1="Player 1";
+var play2="Player 2";
+
+
+function editNames(){
+	play1=prompt("Change"+ play1 +"name");
+	play2=prompt("Change"+play2+"name");
+
+	document.querySelector("h3.Player1").innerHTML=play1;
+	document.querySelector("h3.Player2").innerHTML=play2;
+
+}
+
+function rollDice(){
+	setTimeout(function (){
+		var rand1=Math.floor(Math.random()*6)+1;
+		var rand2=Math.floor(Math.random()*6)+1;
+
+		document.querySelector(".img1").setAttribute("src","images/dice"+rand1+".png");
+		document.querySelector(".img2").setAttribute("src","images/dice"+rand2+".png");
+
+
+		if(rand1 == rand2){
+			document.querySelector("h1").innerHTML="DRAW!";
+		}
+		else if(rand1<rand2){
+			document.querySelector("h1").innerHTML=play2+" WINS!";
+		}
+		else{
+			document.querySelector("h1").innerHTML=play1+" WINS!";
+		}
+
+	},500);
+}
